f08fe9c71157edb0c22a2e223f174baf5e98953a
Mohamed Sakr <mohamedisakr@gmail.com>
2019-02-01 23:34:24 +0200

    Create N-numbers-sum.pas

---
+++ N-numbers-sum.pas
@@ -0,0 +1,32 @@
+// summation of n numbers   
+uses  SysUtils, math;
+var n, sum, t : Integer;
+    inFile : TextFile;
+
+// read from default input 27 21 27 73 2 47 56 49 95 83 0
+begin 
+  sum := 0;
+  Read(n);
+  while n <> 0 do 
+  begin
+    sum := sum + n;
+    Read(n);
+  end;
+  Writeln(sum);
+  Readln;
+end.
+
+// read from file
+begin
+  sum := 0;
+  AssignFile(inFile, 'myfile.txt');
+  Reset(inFile);
+  while not Eof(inFile) do
+  begin
+    Read(inFile, t);
+    sum := sum + t;
+  end;
+  CloseFile(inFile);
+  Writeln(sum);
+  Readln;
+end.
