0dfebe8da74fc973f42dedaa3dd74cf896f8e1c9
Mohamed Sakr <mohamedisakr@gmail.com>
2019-02-07 16:53:12 +0200

    Fibonacci Sequence Without Recursion

--- Fibbonacci.pas
+++ Fibbonacci.pas
@@ -1,5 +1,19 @@
-function calculateFibbonacci(n : Integer):Integer;
+procedure calculateFibbonacci(n : Integer);
+var first, second, next, i : Integer;
+begin
+  first := 1;
+  second := 1;
+  Write(first , ', ' , second , ', ');
+  for i := 1 to n-2 do
+  begin
+    next := first + second;
+    if n-2 = i then Write(next)
+    else Write(next , ', ');
+    first := second;
+    second := next;
+  end;
+end;
 
 
 // Function test 
-// calculateFibbonacci(5)
+calculateFibbonacci(5)
